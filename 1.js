e151e5e7b79516fa7bf06c59da1f8f07f4f92f55
andywijaya15 <andykevonata@gmail.com>
2022-08-25 10:30:03 +0700

    fix rotasi array

--- 1.js
+++ 1.js
@@ -2,12 +2,11 @@
 
 const x = [7, 2, 4, 1, 6, 8, 9];
 const geser = (a, b) => {
-    let result = a;
     for (let i = 0; i < b; i++) {
-        result.unshift(result[result.length - 1]);
-        result = result.slice(0, result.length - 1);
+        a.unshift(a[a.length - 1]);
+        a = a.slice(0, a.length - 1);
     }
-    console.log(result);
+    console.log(a);
 }
 
 geser(x, 3);
